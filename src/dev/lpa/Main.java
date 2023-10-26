2beccc9397974f46a470195938b0df5085da8bc1
pelangan <cristian.urraca@gmail.com>
2023-10-26 23:07:52 +0200

    New update

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -24,7 +24,9 @@ public class Main {
        // gettingElements(placesToVisit);
         //printItinerary(placesToVisit);
         //printItinerary2(placesToVisit);
-        printItinerary3(placesToVisit);
+        //printItinerary3(placesToVisit);
+
+        testIterator(placesToVisit);
 
     }
 
@@ -121,8 +123,25 @@ public class Main {
         }
         System.out.println("Trip ends at " + list.getLast());
 
+    }
 
+    private static void testIterator(LinkedList<String> list) {
 
+        var iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            //     System.out.println(iterator.next());
+            if (iterator.next().equals("Brisbane")) {
+                iterator.add("Lake Wivenhoe");
+            }
+        }
+
+        while (iterator.hasPrevious()) {
+            System.out.println(iterator.previous());
+        }
+        System.out.println(list);
+
+        var iterator2 = list.listIterator(3);
+        System.out.println(iterator2.previous());
     }
 
 
